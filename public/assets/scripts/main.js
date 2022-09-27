0f9bca0f5a7af8c35d636338b324c28b29139446
yamoo9 <yamoo9@naver.com>
2022-09-27 14:48:55 +0900

    add. 함수 추출
    - 세부적인 작은 함수 분리

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -4,7 +4,7 @@ import {
   documentTitle,
   getRandomMinMax,
   text,
-  html,
+  memo,
 } from './lib/index.js';
 
 // 애플리케이션 설정
@@ -15,24 +15,34 @@ const APP_CONFIG = {
   current: 12,
 };
 
+// 카운트 목표 값 설정
+function getTargetCount() {
+  const { min, max } = APP_CONFIG;
+  return getRandomMinMax(min, max);
+}
+
+// 문서 제목 카운트 목표 값 설정
+function updateDocumentTitle(targetCount) {
+  documentTitle((initialDocTitle) => `(${targetCount}) ${initialDocTitle}`);
+}
+
+// UI의 카운트 값 업데이트
+function renderCount(currentCount) {
+  const count = memo(() => $('.Count'), 'Count');
+  text(count, currentCount);
+}
+
 // 애플리케이션 랜딩 초기화
 function randomCountUp() {
-  // 설정 추출
-  const { min, max, current } = APP_CONFIG;
+  const targetCount = getTargetCount();
+  updateDocumentTitle(targetCount);
 
-  // 카운트 목표 값 설정
-  const TARGET_COUNT = getRandomMinMax(min, max);
-
-  // 문서 제목 카운트 목표 값 설정
-  documentTitle((initialDocTitle) => `(${TARGET_COUNT}) ${initialDocTitle}`);
-
-  // UI의 카운트 값 최초 업데이트
-  text(count, current);
+  let { current: count } = APP_CONFIG;
+  renderCount(count);
 }
 
 // 문서 요소 참조
 const startButton = $('.Button');
-const count = $('.Count');
 
 // 애플리케이션 최초 구동
 randomCountUp();

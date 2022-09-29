0698a01562340597add8c2f05a246bbd4781ae17
yamoo9 <yamoo9@naver.com>
2022-09-29 15:22:30 +0900

    homework. 과제 수행
    - throttle.js
    - debounce.js

--- public/assets/scripts/lib/utils/index.js
+++ public/assets/scripts/lib/utils/index.js
@@ -3,5 +3,7 @@ export * from './each.js';
 export * from './loop.js';
 export * from './delay.js';
 export * from './typeOf.js';
+export * from './throttle.js';
+export * from './debounce.js';
 export * from './getRandom.js';
 export * from './throwError.js';

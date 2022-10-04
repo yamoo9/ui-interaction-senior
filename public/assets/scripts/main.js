a01c46b03afd56deec1154068761efea5cab8ccc
yamoo9 <yamoo9@naver.com>
2022-10-04 16:39:16 +0900

    learn. 스네이크 게임
    - 랜덤 사과 그리기
    - 뱀이 사과를 먹었을 때 처리

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -1,19 +1,22 @@
 import {
   addClass,
+  hasClass,
+  removeClass,
   append,
   create,
   each,
   getNode as $,
   loop,
   on,
-  removeClass,
   text,
+  getRandom,
 } from './lib/index.js';
 
 /* -------------------------------------------------------------------------- */
 /* 게임 상수                                                                    */
 /* -------------------------------------------------------------------------- */
 
+const SPEED_RATIO = 0.9;
 const SQUARE_ROWS = 10;
 const SQUARE_COLS = 10;
 const SQUARE_TOTAL = SQUARE_ROWS * SQUARE_COLS;
@@ -23,6 +26,7 @@ const GAME_ELEMENTS = {
   snake: 'Snake',
   apple: 'Apple',
 };
+const SCORE_VALUE = 10;
 const BUTTONS = {
   start: 'Button--start',
   stop: 'Button--stop',
@@ -31,6 +35,7 @@ const BUTTONS = {
 const gridElement = $(`.${GAME_ELEMENTS.grid}`);
 const startButtonElement = $(`.${BUTTONS.start}`);
 const stopButtonElement = $(`.${BUTTONS.stop}`);
+const scoreDisplayElement = $(`.Score__Display`);
 
 /* -------------------------------------------------------------------------- */
 /* 게임 변수                                                                    */
@@ -38,10 +43,12 @@ const stopButtonElement = $(`.${BUTTONS.stop}`);
 
 let squares = [];
 let snake = [2, 1, 0];
-let gameStopId;
 let speed = 1000;
+let score = 0;
 let isStaring = false;
 let direction = 1;
+let gameStopId;
+let appleIndex;
 
 /* -------------------------------------------------------------------------- */
 /* 게임 함수                                                                    */
@@ -61,6 +68,22 @@ function drawSquares({ showGridNumbers = false } = {}) {
   }, SQUARE_TOTAL);
 }
 
+function drawApple() {
+  // 스네이크와 겹치지 않게 그리드 위에 생성되어야 합니다.
+
+  // 스네이크와 겹치지 않는 그리드 위치 찾기
+  // do ~ while
+  do {
+    appleIndex = getRandom(SQUARE_TOTAL - 1);
+  } while (
+    /* 조건: squares 배열의 애플 인덱스(랜덤) 위치의 DOM 요소가 스네이크 클래스 이름을 포함한다면? */
+    hasClass(squares[appleIndex], GAME_ELEMENTS.snake)
+  );
+
+  // 찾은 위치(인덱스)에 해당하는 squares DOM 요소에 애플 클래스 이름을 추가
+  addClass(squares[appleIndex], GAME_ELEMENTS.apple);
+}
+
 function drawSnake() {
   each(snake, (index) => addClass(squares[index], GAME_ELEMENTS.snake));
 }
@@ -73,26 +96,56 @@ function movingSnake() {
   snake.unshift(snake[0] + direction);
   let headIndex = snake[0];
   addClass(squares[headIndex], GAME_ELEMENTS.snake);
+
+  return { headIndex, tailIndex };
 }
 
 function move() {
   if (isGameOver()) {
     return gameOver();
   }
-  movingSnake();
+
+  let { headIndex, tailIndex } = movingSnake();
+
+  // 스네이크 헤드가 애플을 먹으면 처리
+  if (hasClass(squares[headIndex], GAME_ELEMENTS.apple)) {
+    // 애플은 그리드에서 사라져야 함
+    removeClass(squares[appleIndex], GAME_ELEMENTS.apple);
+    // 스네이크의 테일(꼬리)이 길어져야 함
+    addClass(squares[tailIndex], GAME_ELEMENTS.snake);
+    snake.push(tailIndex);
+    // 애플은 그리드의 임의 위치(스네이크와 겹치지 않게) 드로잉
+    drawApple();
+    // 스코어 업
+    scoreUp();
+
+    // 게임 난이도 업 (게임 속도 향상)
+    speed *= SPEED_RATIO;
+    // console.log({ speed });
+  }
+
   gameStopId = setTimeout(move, speed);
+}
+
+function scoreUp() {
+  // 점수 획득
+  // score, SCORE_VALUE
+  score += SCORE_VALUE;
+  text(scoreDisplayElement, score);
 }
 
 function isGameOver() {
   let headIndex = snake[0];
+  let nextHeadIndex = headIndex + direction;
+
   switch (direction) {
     // 벽의 위에 뱀 머리가 충돌
     case -SQUARE_COLS:
-      if (headIndex + direction < 0) return true;
+      if (nextHeadIndex < 0) return true;
       break;
     // 벽의 아래에 뱀 머리가 충돌
     case SQUARE_COLS:
-      if (headIndex + direction >= SQUARE_TOTAL) return true;
+      if (nextHeadIndex >= SQUARE_TOTAL) return true;
       break;
     // 벽의 왼쪽에 뱀 머리가 충돌
     case -1:
@@ -100,11 +153,14 @@ function isGameOver() {
       break;
     // 벽의 오른쪽에 뱀 머리가 충돌
     case 1:
-      console.log(headIndex % SQUARE_COLS);
-      console.log(SQUARE_COLS - 1);
       if (headIndex % SQUARE_COLS === SQUARE_COLS - 1) return true;
   }
   // 뱀의 몸통에 뱀 머리가 충돌
+  // 조건: 뱀의 머리가 이동한 인덱스에 해당하는 DOM 요소에 Snake 클래스 이름이 포함되었다면?
+  if (hasClass(squares[nextHeadIndex], GAME_ELEMENTS.snake)) {
+    return true;
+  }
+
   return false;
 }
 
@@ -126,11 +182,16 @@ function gameStop() {
 }
 
 function init() {
-  drawSquares({ showGridNumbers: true });
+  drawSquares();
   drawSnake();
+  drawApple();
 }
 
 init();
+
+/* -------------------------------------------------------------------------- */
+/* 이벤트 핸들링                                                                 */
+/* -------------------------------------------------------------------------- */
 
 function handleGameStart() {
   if (!isStaring) {

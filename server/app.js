2276a8ca489fe20bd3080f94dafd9d1079ffbbc0
yamoo9 <yamoo9@naver.com>
2022-10-13 16:43:43 +0900

    learn. 템플릿 엔진 EJS 기초

--- server/app.js
+++ server/app.js
@@ -3,22 +3,15 @@ const PORT = process.env.PORT ?? 5000;
 
 const app = express();
 
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res, next) => {
-  res.writeHead(200, 'ok', {
-    'Content-Type': 'text/html; charset=UTF-8',
-  }).end(/* html */ `
-    <!DOCTYPE html>
-      <html lang="ko-KR">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>헬로! 익스프레스</title>
-      </head>
-      <body>
-        <h1>헬로~~ 웹 서버 애플리케이션</h1>
-      </body>
-    </html>
-  `);
+  res.render('index', {
+    title: '헬로!!!',
+  });
 });
+
+// app.use(express.static('public'));
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT} 서버 구동 중...`));
